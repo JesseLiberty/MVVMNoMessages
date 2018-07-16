4a47dffb356f94c3f2aa67cf82f9ad986e46bb50
Jesse Liberty <jesseliberty@gmail.com>
2018-07-16 15:04:17 -0400

    Initial commit

---
+++ MVVMNoMessages/MVVMNoMessages/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVMNoMessages
+{
+	public partial class MainPage : ContentPage
+	{
+      MainPageViewModel vm;
+		public MainPage()
+		{
+			InitializeComponent();
+
+      }
+
+      private async void DeleteEventHandler(int Id)
+      {
+            var answer = await DisplayAlert(
+               "Delete Record?", 
+               $"Do you want to delete record {Id}?", 
+               "Yes", 
+               "No");
+         if (answer==true)
+         {
+            vm.DeleteRecord(Id);
+         }
+      }
+
+      protected override void OnAppearing()
+      {
+         base.OnAppearing();
+         vm = new MainPageViewModel();
+         BindingContext = vm;
+         vm.DeleteEvent += DeleteEventHandler;
+      }
+
+   }
+}
---
+++ MVVMNoMessages/MVVMNoMessages/MainPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVMNoMessages
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+      public ICommand DeleteCommand { get; set; }
+      public int Id { get; set; } = 2;
+      private string output;
+      public string Output
+      {
+         get { return output; }
+         set
+         {
+            output = value;
+            OnPropertyChanged();
+         }
+      }
+
+
+
+      public MainPageViewModel()
+      {
+
+         DeleteCommand = new Command<int>(HandleDelete);
+      }
+
+      public event Action<int> DeleteEvent;
+
+      public void HandleDelete(int Id)
+         
+      {
+         DeleteEvent?.Invoke(Id);
+      }
+
+      public void DeleteRecord(int Id)
+      {
+         Output = String.Format($"Deleted {Id}");
+      }
+      public event PropertyChangedEventHandler PropertyChanged;
+
+
+      protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+
+
+   }
+}
